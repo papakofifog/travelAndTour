f5ee6dd36206f83bc78ae2b14c8f48d782e1a7cb
papakofifog <paakofi275@gmail.com>
2023-03-02 00:59:42 +0000

    Almost done have to fix some some minor erros, and add some Icons for this work to be completed.

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,67 +1,57 @@
 export default[
     {
         id:1,
-        location: "Japan",
-        locat:"https://source.unsplash.com/WLxQvbMyfas",
-        sceneryName:"Mount Fuji",
-        startDate: "12 Jan, 2021",
-        endDate: "12 March, 2021",
-        description:"Mount Fuji is perhaps Japan’s biggest attraction, being the country’s highest peak at an impressive 3,776 metres. "+ 
-        "The official climbing season for Mount Fuji is from early July until early-September when the trails are free of snow "+
-        "and the weather is mild, so it is the perfect time to visit if you are eager to take on a rewarding and memorable challenge this summer.",
-        locationURL:""
+        location: "South Africa",
+        image:" http://tasteofdurban.co.za/wp-content/uploads/2017/10/uShaka-Marine-World.jpg ",
+        sceneryName:"Ushaka Durban",
+        startDate: "12 Dec, 2014",
+        endDate: "12 Jan, 2014",
+        description:"The uShaka Marine World comprises of 5 sections: uShaka Sea World, uShaka Wet ‘n Wild, uShaka Beach, uShaka Village Walk and, uShaka Kids World. The entertainment is non-stop, extending from the daytime operations of Sea World, Wet â€˜n Wild, and uShaka Beach, to the night time festivities of the Village Walk.",
+        locationURL:"https://www.google.com/maps/place/uShaka+Marine+World/@-29.8672537,31.0436655,17z/data=!3m1!4b1!4m6!3m5!1s0x1ef7a85a762770cb:0xd5afb503d2f2fd5a!8m2!3d-29.8672537!4d31.0458542!16zL20vMDUxdmg4?hl=en"
     },
 
     {   
         id:2,
-        location: "Japan",
-        image:"https://source.unsplash.com/WLxQvbMyfas",
-        sceneryName:"Mount Fuji",
-        startDate: "12 Jan, 2021",
-        endDate: "12 March, 2021",
-        description:"Mount Fuji is perhaps Japan’s biggest attraction, being the country’s highest peak at an impressive 3,776 metres. "+ 
-        "The official climbing season for Mount Fuji is from early July until early-September when the trails are free of snow "+
-        "and the weather is mild, so it is the perfect time to visit if you are eager to take on a rewarding and memorable challenge this summer.",
-        locationURL:""
+        location: "Nigeria",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/NAIA_Abuja_Terminal_Entrance.jpg/250px-NAIA_Abuja_Terminal_Entrance.jpg",
+        sceneryName:"Nnamdi Azikiwe International Airport",
+        startDate: "12 April, 2016",
+        endDate: "12 July, 2016",
+        description:"Nnamdi Azikiwe International Airport is an international airport serving Abuja, in the Federal Capital Territory of Nigeria. It is the main airport serving the Nigerian capital city and was named after Nigeria's first President, Nnamdi Azikiwe.",
+        locationURL:"https://www.google.com/maps/place/Nnamdi+Azikiwe+International+Airport/@9.0060558,7.2673137,17z/data=!3m1!4b1!4m6!3m5!1s0x104e7195d1abacc9:0xc905ee88d56fa2ff!8m2!3d9.0060505!4d7.2695024!16zL20vMDRuYzJf?hl=en"
     },
 
     {   
         id:3,
-        location: "Japan",
-        image:"https://source.unsplash.com/WLxQvbMyfas",
-        sceneryName:"Mount Fuji",
-        startDate: "12 Jan, 2021",
-        endDate: "12 March, 2021",
-        description:"Mount Fuji is perhaps Japan’s biggest attraction, being the country’s highest peak at an impressive 3,776 metres. "+ 
-        "The official climbing season for Mount Fuji is from early July until early-September when the trails are free of snow "+
-        "and the weather is mild, so it is the perfect time to visit if you are eager to take on a rewarding and memorable challenge this summer.",
-        locationURL:""
+        location: "Nigeria",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/MMIA_Intl_Terminal_2019.jpg/250px-MMIA_Intl_Terminal_2019.jpg",
+        sceneryName:"Murtala Muhammed International Airport",
+        startDate: "12 March, 2016",
+        endDate: "12 June, 2016",
+        description:"The airport was built during World War II. West African Airways Corporation was formed in 1947 and had its main base at Ikeja. De Havilland Doves were initially operated on WAACs Nigerian internal routes then West African services.",
+        locationURL:"https://www.google.com/maps?q=lagos+international+airport&bih=643&biw=1366&client=firefox-b-d&hl=en&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiX3v_pgLz9AhVEuKQKHWECAbsQ_AUoAXoECAEQAw"
     },
 
     {   
         id:4,
-        location: "Japan",
-        image:"https://source.unsplash.com/WLxQvbMyfas",
-        sceneryName:"Mount Fuji",
-        startDate: "12 Jan, 2021",
-        endDate: "12 March, 2021",
-        description:"Mount Fuji is perhaps Japan’s biggest attraction, being the country’s highest peak at an impressive 3,776 metres. "+ 
-        "The official climbing season for Mount Fuji is from early July until early-September when the trails are free of snow "+
-        "and the weather is mild, so it is the perfect time to visit if you are eager to take on a rewarding and memorable challenge this summer.",
-        locationURL:""
+        location: "Ghana",
+        image:"https://www.graphic.com.gh/images/2022/jun/17/Takoradi%20market.jpg",
+        sceneryName:"Takoradi",
+        startDate: "12 Nov, 2022",
+        endDate: "12 Dec, 2022",
+        description:"Sekondi-Takoradi is a city in Ghana comprising the twin cities of Sekondi and Takoradi.[2] It is the capital of Sekondi – Takoradi Metropolitan Assembly and the Western Region of Ghana. Sekondi-Takoradi is the region’s and largest Fante city as well as an industrial and commercial center, with a population of 445,205 people (2012).",
+        locationURL:"https://www.google.com/maps/place/Takoradi/@4.9147493,-1.8125305,13z/data=!3m1!4b1!4m6!3m5!1s0xfe7799a7b4d8bd9:0xd5df62ba6ac74520!8m2!3d4.9015794!4d-1.7830973!16zL20vMDNsNG52?hl=en"
     },
 
     {   
         id:5,
-        location: "Japan",
-        image:"https://source.unsplash.com/WLxQvbMyfas",
-        sceneryName:"Mount Fuji",
-        startDate: "12 Jan, 2021",
-        endDate: "12 March, 2021",
-        description:"Mount Fuji is perhaps Japan’s biggest attraction, being the country’s highest peak at an impressive 3,776 metres. "+ 
-        "The official climbing season for Mount Fuji is from early July until early-September when the trails are free of snow "+
-        "and the weather is mild, so it is the perfect time to visit if you are eager to take on a rewarding and memorable challenge this summer.",
-        locationURL:""
+        location: "Ghana",
+        image:"https://blog.getrooms.co/wp-content/uploads/2020/12/knust-entrance-1536x768.jpg",
+        sceneryName:"KNUST",
+        startDate: "12 Jan, 2022",
+        endDate: "12 March, 2022",
+        description:"Kwame Nkrumah University of Science and Technology (KNUST) is one of the main public or government universities in Ghana. The mission of KNUST is to improve knowledge in science and technology through creating a friendly environment for undertaking relevant research, quality teaching, entrepreneurship training and community engagement to improve the quality of life of all students and staff. The university of science and technology was established by a government ordinance on 6th October 1951. It was, however, opened officially on 22nd January 1952. It is the biggest university in Kumasi, the capital town of the Ashanti region. With every academic institution comes courses and requirements for entry. This article would delve into the courses offered at KNUST, the cut-off grade for KNUST, whether KNUST accepts D7 and C6, and finally KNUST courses, requirements and fees.",
+        locationURL:"https://www.google.com/maps/place/Kwame+Nkrumah+University+of+Science+and+Technology/@6.6745088,-1.5738001,17z/data=!3m1!4b1!4m6!3m5!1s0xfdb9461ea6fb9af:0xb0da4f6d9c4b361f!8m2!3d6.6745035!4d-1.5716114!16zL20vMDd4bHpf?hl=en"
     }
 
 ]
