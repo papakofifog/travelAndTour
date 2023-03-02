9545d06217e1bf10eaa88a74be9b74029242ce76
papakofifog <paakofi275@gmail.com>
2023-03-02 08:39:12 +0000

    Final changes have been made

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -6,7 +6,7 @@ export default function Card (props){
         <div className="card">
             <img className="card--image" src={props.image} alt="" />
             <div className="card--body">
-                <p className="card--Location"><span className="card--travelLocation--Icon"><IoLocationSharp color="orange" /></span>{props.location} <a href={props.locationURL} className="googleLocation">View on Google Maps</a></p>
+                <p className="card--Location"><span className="card--travelLocation--Icon"></span>{props.location} <a href={props.locationURL} className="googleLocation"><IoLocationSharp className="locationIcon"  /></a></p>
                 <h4 className="card--travelSceneryName bold">{props.sceneryName}</h4>
                 <p className="card--travelDuration bold">{props.startDate} - {props.endDate}</p>
                 <p className="card-travelDescription">{props.description}</p>
